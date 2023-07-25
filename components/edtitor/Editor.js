0607b1614e21ee1394e739e8fa6f54ed722b7a33
Musili Brian <musilibrian07@gmail.com>
2023-07-25 14:43:01 +0300

    ssr

--- components/edtitor/Editor.js
+++ components/edtitor/Editor.js
@@ -1,13 +1,22 @@
-// components/TextEditor.js
-"use client";
-
-import React from "react";
+import React, { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
-import ReactQuill from "react-quill";
+
+const DynamicReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 const TextEditor = ({ value, onChange }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // Return null on the server-side to avoid rendering the editor
+  }
+
   return (
-    <ReactQuill
+    <DynamicReactQuill
       value={value}
       onChange={onChange}
       theme="snow" // Use the "snow" theme, which matches the Tailwind CSS styling
